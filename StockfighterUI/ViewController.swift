29143e459ba1e262e8faac1b522a8bfba5074794
Alexey Bilous <alexicewind@gmail.com>
2015-12-17 01:10:48 -0800

    Initial Commit

---
+++ StockfighterUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StockfighterUI
+//
+//  Created by Oleksii BIlous on 12/17/15.
+//  Copyright © 2015 Oleksii BIlous. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
